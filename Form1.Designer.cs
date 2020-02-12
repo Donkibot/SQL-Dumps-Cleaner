6751945822f58d1522d7ccf8f95ee50ef397f378
Denis Pavlyuchenko <tarelkin29@gmail.com>
2020-02-12 17:08:58 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderTextBox = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.White;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 28);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.richTextBox1.Size = new System.Drawing.Size(650, 306);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(507, 340);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 35);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "DELETE ALL";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(12, 340);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 35);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Show all files";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // folderTextBox
+            // 
+            this.folderTextBox.Location = new System.Drawing.Point(13, 2);
+            this.folderTextBox.Name = "folderTextBox";
+            this.folderTextBox.Size = new System.Drawing.Size(616, 20);
+            this.folderTextBox.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(635, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(26, 20);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(260, 340);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(155, 35);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Open in Folder";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 387);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.folderTextBox);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "ClearSqlDumps";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox folderTextBox;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        DirectoryInfo d;
+        FileInfo[] Files;
+
+        long[] fileSize;
+        long overalSize = 0;
+        string[] FileArray;
+        string FilePath;
+
+        public Form1()
+        {
+            InitializeComponent();
+            FilePath = @"D:\Programs\Microsoft SQL\MSSQL15.MSSQLSERVER\MSSQL\Log\Polybase\dump";
+            UpdateFileArray();
+            
+    }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UpdateFileArray();
+            ClearTextBox();
+            WriteInTextBox();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DeleteAllFiles();
+            UpdateFileArray();
+            ClearTextBox();
+            WriteInTextBox();
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Process.Start("explorer.exe", FilePath);
+        }
+
+        void ClearTextBox()
+        {
+            if (richTextBox1.Text != "")
+            {
+               richTextBox1.Clear();
+            }
+        }
+
+        void WriteInTextBox()
+        {
+            foreach (string str in FileArray)
+            {
+                richTextBox1.Text += str + "\n";
+            }
+            string overalSizeInGb = string.Format("\nOveral size = {0:0.00} Gb\r\n", getOveralSize() / 1000.0);
+            richTextBox1.Text += overalSizeInGb;
+        }
+
+        long getOveralSize()
+        {
+            overalSize = 0;
+            foreach (long l in fileSize)
+            {
+                overalSize += l;
+            }
+            return overalSize;
+        }
+
+
+        void DeleteAllFiles()
+        {
+            foreach (FileInfo file in Files)
+            {
+                file.Delete();
+            }
+        }
+
+        void UpdateFileArray()
+        {
+            DirectoryInfo d = new DirectoryInfo(FilePath);
+            Files = d.GetFiles();
+            FileArray = new string[Files.Length];
+            fileSize = new long[Files.Length];
+
+            int count = 0;
+            foreach (FileInfo file in Files)
+            {
+                fileSize[count] = file.Length / (1024 * 1024);
+                string temp = $"{file.Name} - {fileSize[count]}mb";
+                FileArray[count] = temp;
+                count++;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK) {
+                string path = folderBrowserDialog1.SelectedPath;
+                folderTextBox.Text = path;
+                FilePath = path;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            folderTextBox.Text = FilePath;
+        }
+    }
+
+}
